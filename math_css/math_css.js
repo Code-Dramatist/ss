6a623ec504a963a74c17a40883add15f369e173c
Code-Dramatist <lu982524190@gmail.com>
2022-08-20 15:47:18 +0800

    done

--- math_css/math_css.js
+++ math_css/math_css.js
@@ -4,11 +4,8 @@ function insertStr(soure, start, newStr){
  }
 let obj = $response.body
 //var obj1 = obj.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">')
-let newStr = '<link rel="stylesheet" href="https://gd-cowtransfer.cdn.cowtransfer.com/cowtransfer/cowtransfer/97349/d9d094a73eab45cea454c4e3fcb3cd9e.css?response-content-disposition=attachment; filename=math%281%29.css;filename*=utf-8\'\'math%281%29.css&auth_key=1660992996-d427d85f700c4d1b838541a5e13320c5-0-eba48e4059c6fe23b3089e12fc68b850&user_id=1021095091821497300&biz_type=1&channel_code=COW_CN_WEB&business_code=COW_TRANSFER" type="text/css">'
+let newStr = '<link rel="stylesheet" href="https://gd-cowtransfer.static.cowtransfer.com/cowtransfer/cowtransfer/97349/50d1f177b02745acbc96d1de3e90a906.css?response-content-disposition=attachment; filename=math.css;filename*=utf-8\'\'math.css&auth_key=1660995944-6ddfe82cf16b48adb52f1f2731002719-0-9465a6aabcccf65132bc8427c536d149&channel_code=COW_CN_WEB&business_code=COW_TRANSFER&biz_type=1&user_id=1021095091821497349" type="text/css">'
 var start = obj.search(/<script type="text\/javascript"/)
 console.log(start)
-var obj1 = insertStr(obj,start,newStr)
-//console.log(obj1)
+var obj1 = insertStr(obj,start,newStr).replace(/<div id="popup">[\s\S]*?<div class="text">[\s\S]*?<\/div>[\s\S]*?<\/div>/)
 $done({body:obj1});
-
-//$done({body: obj1});
