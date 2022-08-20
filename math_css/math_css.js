4f3e9ec1bd3d16a0236449773e9a77e5b0f49466
Code-Dramatist <lu982524190@gmail.com>
2022-08-20 12:34:24 +0800

    cs

--- math_css/math_css.js
+++ math_css/math_css.js
@@ -6,6 +6,7 @@ let obj = $response.body
 var obj1
 //var obj1 = obj.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">')
 var start = obj.search(/<script type="text\/javascript"/)
+console.log(start)
 insertStr(obj,start,obj1)
 console.log(obj1)
 $done({body:obj1});
