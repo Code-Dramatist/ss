b752268fb2828da6ce33f218cba130c069e1831a
Code-Dramatist <lu982524190@gmail.com>
2022-08-20 11:44:10 +0800

    查看log

--- math_css/math_css.js
+++ math_css/math_css.js
@@ -1,6 +1,7 @@
 
 let obj = $response.body
 var obj1 = obj.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">')
+console.log(obj1)
 $done({body:obj1});
 
 //$done({body: obj1});
