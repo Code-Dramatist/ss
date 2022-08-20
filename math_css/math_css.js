bba6a4812e39ebf5261c9e2ad65a93c2b52d39c8
Code-Dramatist <lu982524190@gmail.com>
2022-08-20 11:34:58 +0800

    css 修改

---
+++ math_css/math_css.js
@@ -0,0 +1,7 @@
+
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">');
+
+$done({ body });
+
+//$done({body: obj1});
