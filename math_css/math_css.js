8bf7c0627757e93e0ec82ebba90e8e35d08c50da
Code-Dramatist <lu982524190@gmail.com>
2022-08-20 11:42:32 +0800

    bug

--- math_css/math_css.js
+++ math_css/math_css.js
@@ -1,6 +1,6 @@
 
-var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">');
-
-$done({body});
+let obj = $response.body
+var obj1 = obj.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">')
+$done({body:obj1});
 
 //$done({body: obj1});
