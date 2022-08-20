39b9b043d86ff8f88f6c3c5e73d0d09eef1b8d8a
Code-Dramatist <lu982524190@gmail.com>
2022-08-20 12:39:26 +0800

    cs

--- math_css/math_css.js
+++ math_css/math_css.js
@@ -3,11 +3,11 @@ function insertStr(soure, start, newStr){
     return soure.slice(0, start) + newStr + soure.slice(start);
  }
 let obj = $response.body
-var obj1
 //var obj1 = obj.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">')
+let newStr = '<link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">'
 var start = obj.search(/<script type="text\/javascript"/)
 console.log(start)
-insertStr(obj,start,obj1)
+var obj1 = insertStr(obj,start,newStr)
 console.log(obj1)
 $done({body:obj1});
 
